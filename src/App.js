13ef744328c82b06983bb124408148d990444a8d
bejan afshar <cosmopolit_b@hotmail.de>
2021-05-22 15:18:30 +0200

    deleted css files, installed styled components, fetched api with useEffects and useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeConsumer } from 'styled-components';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((results) => results.json())
+      .then((data) =>
+        setCharacters(
+          data.results.map((character) => {
+            return character;
+          })
+        )
+      );
+  }, []);
+
+  console.log(characters);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainWrapper></MainWrapper>
     </div>
   );
 }
 
 export default App;
+
+const MainWrapper = styled.div``;
